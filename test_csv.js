c55dc2ed7e9c0f55c29cd8cf24e136b055c79d91
Ponlapat <janintaponlapat@gmail.com>
2026-01-06 16:50:54 +0700

    options for test new payroll

--- test_csv.js
+++ test_csv.js
@@ -6,15 +6,34 @@ import { scenario } from 'k6/execution'; // ใช้สำหรับดึง
 import encoding from 'k6/encoding';
 
 // 1. กำหนด Options (สำคัญมากเพื่อให้มีข้อมูลรัน)
+/* export const options = {
+    scenarios: {
+        deadlock_battle: {
+            executor: 'constant-arrival-rate',
+            rate: 200,            // ต้องการ 200 requests
+            timeUnit: '1s',       // ภายใน 1 วินาที (คือยิง 200 req/sec)
+            duration: '5s',      // ยิงต่อเนื่อง 5 วินาที
+            preAllocatedVUs: 200, // เตรียมคนไว้รอเลย 200 คน
+            maxVUs: 1000,          // ถ้าคนไม่พอให้ขยายได้ถึง 1000
+        },
+    },
+}; */
+
 export const options = {
-    vus: 1,            // จำนวนคนจำลอง
-    iterations: 1,    // จำนวนครั้งที่จะรันทั้งหมด (หารเฉลี่ยกันในหมู่ VUs)
+    scenarios: {
+        spike_once: {
+            executor: 'per-vu-iterations',
+            vus: 100,          // ปล่อยคนออกมา x คนพร้อมกัน
+            iterations: 1,     // ทุกคนยิงแค่คนละ 1 ครั้งแล้วจบงาน
+            maxDuration: '60s', // กำหนดเวลาสูงสุดที่ยอมให้รัน
+        },
+    },
 };
 
 // 2. โหลดไฟล์ข้อมูล CSV
 const csvData = new SharedArray('csv data', function () {
     // ต้องมั่นใจว่าไฟล์ .csv อยู่ที่เดียวกับไฟล์ .js นี้
-    return papaparse.parse(open('./scenario_1.csv'), { header: true }).data;
+    return papaparse.parse(open('./scenario_4.csv'), { header: true }).data;
 });
 
 export default function () {
@@ -27,16 +46,22 @@ export default function () {
     const encodedInstanceServerId = encoding.b64encode(row.instance_server_id);
     const encodedInstanceServerChannelId = encoding.b64encode(row.instance_server_channel_id);
 
-    const url = 'http://localhost:3001';
+    const url = 'https://hms-php-core-payroll.azurewebsites.net/api-web.php';
 
     const payload = JSON.stringify({
         _compgrp: 'hrs',
         _comp: 'calculation_normal',
-        _action: 'calculate_month',
+        _action: 'calculate_payroll',
         year_month: row.year_month,
         employee_id: encodedEmployeeId,
+        calculate_person: 'calculate_person',
+        calculate_to: 'NOW',
         instance_server_id: encodedInstanceServerId,
         instance_server_channel_id: encodedInstanceServerChannelId,
+        identify_user_id: 'MjAyMjExMDJFQzRGNUJDNTZGRjg=',
+        language_code: 'TH',
+        user_name: '',
+        user_psw: '',
     });
 
     const params = {
@@ -51,5 +76,5 @@ export default function () {
         'status is 200': (r) => r.status === 200,
     });
 
-    sleep(1);
+    // sleep(1);
 }
